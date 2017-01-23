613875396b0cb691d3666f7671d721221e491155
Muhammad Fauzil Haqqi <me@haqqi.net>
2017-01-23 21:21:19 +0700

    [add] configuration for urlManager

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,23 @@
 <?php
+
+use yii\web\UrlNormalizer;
+
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
+  'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+  'components' => [
+    'urlManager'   => [
+      'enablePrettyUrl' => true,
+      'showScriptName'  => false,
+      'normalizer' => [
+        'class' => UrlNormalizer::className(),
+        'action' => UrlNormalizer::ACTION_REDIRECT_PERMANENT
+      ],
+      'rules'           => [
+        // rules described in each app
+      ],
     ],
+    'cache' => [
+      'class' => 'yii\caching\FileCache',
+    ],
+  ],
 ];

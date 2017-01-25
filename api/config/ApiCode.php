a703c210bcc515fac73d596b1aecad9df13a4db7
_alvian <alvianthelfarqy@gmail.com>
2017-01-25 10:15:37 +0700

    [add] api code class

---
+++ api/config/ApiCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace api\config;
+
+use yii\base\Object;
+
+class ApiCode extends Object {
+    /**
+     * ApiCode for auth
+     */
+    const REGISTER_SUCCESS = 10;
+    const REGISTER_FAILED = 11;
+
+}

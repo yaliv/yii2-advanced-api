4bfc1058743d09f246707011f50af4c1333d3157
_alvian <alvianthelfarqy@gmail.com>
2017-01-25 10:41:27 +0700

    [update] register function

--- api/modules/v1/controllers/AuthController.php
+++ api/modules/v1/controllers/AuthController.php
@@ -2,7 +2,13 @@
 
 namespace api\modules\v1\controllers;
 
+use api\config\ApiCode;
+use api\forms\RegisterUserForm;
+use common\exceptions\BetterHttpException;
+use yii\base\InvalidConfigException;
+use yii\base\InvalidParamException;
 use yii\rest\Controller;
+use yii\web\HttpException;
 
 /**
  * Class AuthController
@@ -16,10 +22,10 @@ class AuthController extends Controller
         $behaviors = parent::behaviors();
 
         $behaviors['authenticator']['except'] = [
-            'login',
-            'forgot-password',
-            'reset-password',
-            'register'
+          'login',
+          'forgot-password',
+          'reset-password',
+          'register'
         ];
 
         return $behaviors;
@@ -28,18 +34,45 @@ class AuthController extends Controller
     protected function verbs()
     {
         return [
-            'login'           => ['post'],
-            'logout'          => ['post'],
-            'forgot-password' => ['post'],
-            'reset-password'  => ['get', 'post'],
-            'register'        => ['post'],
-            'change-password' => ['post']
+          'login'           => ['post'],
+          'logout'          => ['post'],
+          'forgot-password' => ['post'],
+          'reset-password'  => ['get', 'post'],
+          'register'        => ['post'],
+          'change-password' => ['post']
         ];
     }
 
     public function actionRegister()
     {
-        return [];
+        try {
+            // registration primary form. no need to login!!
+            $userForm = new RegisterUserForm(\Yii::$app->request->headers->get('X-Device-identifier'));
+        } catch (InvalidConfigException $e) {
+            throw new HttpException(400, $e->getMessage(), ApiCode::DEVICE_IDENTIFIER_NOT_FOUND);
+        }
+
+        $userForm->load(\Yii::$app->request->post(), 'User');
+
+        if ($userForm->validate()) {
+
+            $user = $userForm->register();
+
+            return [
+              'name'    => 'Success',
+              'message' => 'Registration as User of ' . $user->username . ' success.',
+              'code'    => ApiCode::REGISTER_SUCCESS,
+              'status'  => 200,
+              'data'    => $user->toArray([
+                  'username',
+                  'email',
+              ])
+            ];
+        }
+
+        throw new BetterHttpException(400, 'Registration failed.', [
+          'User' => $userForm->getErrors()
+        ], ApiCode::REGISTER_FAILED);
     }
 
     public function actionLogin()

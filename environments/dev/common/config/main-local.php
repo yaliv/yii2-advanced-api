e492ed8e9e269bdf60c354e236f7d4901dcd29f9
Muhammad Yahya Muhaimin <surat.pribadiku@gmail.com>
2017-01-25 16:56:02 +0700

    [update] default dbname & saran penggunaan tablePrefix.

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,10 +3,11 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=mydb',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
+            'tablePrefix' => 'tbl_',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',

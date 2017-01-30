d165f212834b50ba901d18cd19125a41cb9d9722
_alvian <alvianthelfarqy@gmail.com>
2017-01-30 10:28:22 +0700

    [add] scenario for username

--- common/forms/ForgotPasswordForm.php
+++ common/forms/ForgotPasswordForm.php
@@ -10,28 +10,47 @@ use yii\base\Model;
  * @package api\forms
  *
  */
-class ForgotPasswordForm extends Model {
+class ForgotPasswordForm extends Model
+{
 
-    const SCENARIO_FORGOT_LOGIN_USERNAME = 'submitLoginUsername';
-    const SCENARIO_FORGOT_LOGIN_EMAIL    = 'submitLoginEmail';
-  /**
-   * @var $email
-   */
-  public $email;
+    /**
+     * const SCENARIO_FORGOT_LOGIN_USERNAME
+     */
+    const SCENARIO_FORGOT_LOGIN_USERNAME = 'forgotLoginUsername';
+    const SCENARIO_FORGOT_LOGIN_EMAIL    = 'forgotLoginEmail';
+    /**
+     * @var $email
+     */
+    public $email;
 
-  public function rules() {
-    return [
-      [['email'], 'required','on' => self::SCENARIO_FORGOT_LOGIN_EMAIL],
-      [['email'], 'trim'],
-      [['email'], 'email'],
-      ['email', 
-       'exist',
-       'targetClass' => User::className(),
-       'message' => 'Email / user does not exist',
-       'filter' => ['status' => User::STATUS_ACTIVE]
-      ],
-    ];
-  }
+    /**
+     * @var $username
+     */
+    public $username;
+
+    public function rules()
+    {
+        return [
+          [['email'], 'required', 'on' => self::SCENARIO_FORGOT_LOGIN_EMAIL],
+          [['username'], 'required', 'on' => self::SCENARIO_FORGOT_LOGIN_USERNAME],
+          [['email', 'username'], 'trim'],
+          [['email'], 'email'],
+          [
+            'email',
+            'exist',
+            'targetClass' => User::className(),
+            'message'     => 'Email does not exist',
+            'filter'      => ['status' => User::STATUS_ACTIVE]
+          ],
+          [
+            'username',
+            'exist',
+            'targetClass' => User::className(),
+            'message'     => 'Username does not exist',
+            'filter'      => ['status' => User::STATUS_ACTIVE]
+          ],
+        ];
+    }
 
     public function scenarios()
     {
@@ -41,14 +60,21 @@ class ForgotPasswordForm extends Model {
 
         return $scenarios;
     }
-  
-  public function getUser() {
-    $user = User::findOne([
-      'email' => $this->email,
-      'status' => User::STATUS_ACTIVE
-    ]);
-    
-    return $user;
-  }
+
+    public function getUser()
+    {
+        if ($this->scenario === self::SCENARIO_FORGOT_LOGIN_USERNAME) {
+//            return $class::findByUsername($this->username);
+            return User::findOne([
+              'username' => $this->username,
+              'status'   => User::STATUS_ACTIVE
+            ]);
+        }
+
+        return User::findOne([
+          'email'  => $this->email,
+          'status' => User::STATUS_ACTIVE
+        ]);
+    }
 
 }

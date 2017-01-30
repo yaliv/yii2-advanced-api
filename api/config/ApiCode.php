e344b13f9a0e17d29952e33683e69bc930402622
_alvian <alvianthelfarqy@gmail.com>
2017-01-30 10:15:00 +0700

    [add] api code forgot password

--- api/config/ApiCode.php
+++ api/config/ApiCode.php
@@ -9,11 +9,13 @@ class ApiCode extends Object
     /**
      * ApiCode for auth
      */
-    const REGISTER_SUCCESS = 10;
-    const REGISTER_FAILED  = 11;
-    const LOGIN_SUCCESS    = 12;
-    const LOGIN_FAILED     = 13;
-    const LOGOUT_SUCCESS   = 14;
+    const REGISTER_SUCCESS        = 10;
+    const REGISTER_FAILED         = 11;
+    const LOGIN_SUCCESS           = 12;
+    const LOGIN_FAILED            = 13;
+    const LOGOUT_SUCCESS          = 14;
+    const FORGOT_PASSWORD_SUCCESS = 15;
+    const FORGOT_PASSWORD_FAILED  = 16;
 
     /**
      * ApiCode for invalid configuration
